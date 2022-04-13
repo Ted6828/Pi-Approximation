4c97b92d70102c1f19434f54474b545e03fccd0c
Ted6828 <77334264+Ted6828@users.noreply.github.com>
2022-04-13 15:37:42 +0100

    Added Gregory-Leibniz Series

--- Pi-Approximations.py
+++ Pi-Approximations.py
@@ -9,7 +9,7 @@ def piCalc(recur):
         else:
             operator = +1
         den = den + 2
-    return(pi)
+    return pi
 
 def piCalc2(total):
     count = 0
@@ -20,5 +20,18 @@ def piCalc2(total):
             count = count + 1
     return str(4 * count / total)
 
-print("Pi using Nilakantha Series:", piCalc(500000))
-print("Pi using Monte Carlo Method:", piCalc2(500000))
+def piCalc3(recur):
+    pi, den, operator = 0, 1, 1
+    for i in range(0, recur):
+        pi = (pi) + (operator * 4 / (den))
+        if operator == 1:
+            operator = -1
+        else:
+            operator = +1
+        den = den + 2
+    return pi
+
+precision = int(input("Enter precision... : "))
+print("Pi using Nilakantha Series:", piCalc(precision))
+print("Pi using Gregory-Leibniz series:", piCalc3(precision))
+print("Pi using Monte Carlo Method:", piCalc2(precision))
